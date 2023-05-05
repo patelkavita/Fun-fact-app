67df75effa98cf87da6233c3dd42410541c680e2
Kavita <patelkavita@users.noreply.github.com>
2023-05-05 13:16:19 +0530

    Added Functionality to hide and show form using JavaScript

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+console.log("hello");
+
+const btn = document.querySelector(".btn-opn");
+const form = document.querySelector(".fact-form");
+btn.addEventListener("click", function() {
+    if(form.classList.contains("hidden")) {
+        form.classList.remove("hidden");
+        btn.textContent = "Close";
+    } else {
+        form.classList.add("hidden");
+        btn.textContent = "Share a fact";
+    }
+});
