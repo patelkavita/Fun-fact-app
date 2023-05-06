08c856f3aa7ae6d028d81e40dd03febe57a93a2e
Kavita <patelkavita@users.noreply.github.com>
2023-05-06 23:05:57 +0530

    Added message to add fact, if there is no fact in a particular category

--- src/App.js
+++ src/App.js
@@ -220,6 +220,10 @@ function CategoryFilter(props) {
 
 function FactList(props) {
  const {facts} = props;
+ 
+ if(facts.length === 0) {
+  return <p className="message">No Facts for this category yet!. Create the first one✌️</p>
+ }
 
   return ( <section> <ul className="facts-list">  {
     facts.map((fact) => (
